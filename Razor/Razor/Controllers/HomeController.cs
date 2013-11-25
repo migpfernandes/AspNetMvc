15a4ed7317c752dc434064b74d3f368e6dcaff3e
Miguel Fernandes <migpfernandes@gmail.com>
2013-11-25 00:21:45 +0000

    Inicio Tutorial razor

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpression() {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
+    }
+}
