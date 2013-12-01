78f604c4ac8dea7951adf0e983af2deb71762d68
Miguel Fernandes <migpfernandes@gmail.com>
2013-12-01 22:30:27 +0000

    End Chapter 7: Moq, Ninject and Unit Test

---
+++ EssentialTools/EssentialTools/Models/Discount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTools.Models
+{
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal discountSize;
+
+        public DefaultDiscountHelper(decimal discountParam)
+        {
+            discountSize = discountParam;
+        }
+
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (discountSize / 100M * totalParam));
+        }
+    }
+}
